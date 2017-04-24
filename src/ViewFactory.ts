548343f3fd5eed83e71b0c469198daafd5ae4a15
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2017-04-24 16:35:27 +0200

    Updaed ViewFactory  and main scenario

--- src/ViewFactory.ts
+++ src/ViewFactory.ts
@@ -7,18 +7,31 @@ import {
     createOverrideContext
 } from 'aurelia-framework';
 
+/** A custom element Factory */
 @autoinject()
 export class ViewFactory {
 
+    /**
+     * Creates a new ViewFactory
+     */
     constructor(private viewCompiler: ViewCompiler, private container: Container) {
     }
 
-    insert(containerElement, html, viewModel, resources: ViewResources) {
+    /**
+     * Compile and insert a new custom element (or plain HTML) to the given container element.
+     * @param containerElement The html container element that will host the new compiled component
+     * @param html The row HTML code to compile (could be plain HTML or aurelia custom element, attribute and so on)
+     * @param viewModel The view model that will be used to bind properties and functions to the new compiled component
+     * @param resources The ViewResources of the new component (the <require from='...'> that must be used to use this component)
+     * @returns A function to call when the gust component should be detached (remove and unbind the gust component)
+     */
+    insert(containerElement: Element, html: string, viewModel: any, resources: ViewResources) {
         let viewFactory = this.viewCompiler.compile(html, resources);
         let view = viewFactory.create(this.container);
         let viewSlot = new ViewSlot(containerElement, true);
         viewSlot.add(view);
         view.bind(viewModel, createOverrideContext(viewModel));
+        viewSlot.attached();
         return () => {
             viewSlot.remove(view);
             view.unbind();
--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,6 @@
-import { bindable,inlineView } from 'aurelia-framework';
+import { bindable, inlineView, bindingMode, TaskQueue } from 'aurelia-framework';
 import { Container, autoinject } from 'aurelia-dependency-injection';
-import { viewResources, ViewResources, ViewEngine } from 'aurelia-framework';
+import { ViewResources, ViewEngine } from 'aurelia-framework';
 import { ViewFactory } from './ViewFactory';
 
 import { IDynamic } from './IDynamic';
@@ -9,30 +9,71 @@ import { IMyComponent } from './IMyComponent';
 @inlineView(`<template></template>`)
 @autoinject()
 export class Component implements IMyComponent {
-    @bindable()
-    name = 'Component';
 
-    @bindable()
-    log: () => void = Function;
+    /** IMyComponent implementation */
+    @bindable() name = 'Component';
+    @bindable() log: () => void = Function;
 
-    /* The guet view model injected instance */
-    private gustVm: IDynamic;
+    /** The guest's view model reference */
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) public instance: any;
+    /** The guest's view instance (not an HTML Element) */
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) public view: any;
+    /** The guest's HTML Element view */
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) public element: Element;
 
-    constructor(private viewFactory: ViewFactory, private viewEngine: ViewEngine, private viewResources: ViewResources, private host: Element) {
-        let type = Container.instance.get("Component");
-        this.gustVm = new type();
+    /** Called when the data is bound */
+    @bindable() onDatabound: () => void = Function;
+
+    /** The injected guest component information */
+    private dynamic: IDynamic;
+
+    /** Removes the view slot and detach the injected components */
+    private remove: () => void;
+
+    /** Inject required services and resolve 'Component' */
+    constructor(private viewFactory: ViewFactory, private viewEngine: ViewEngine, private viewResources: ViewResources, private host: Element, private taskQueue: TaskQueue) {
+        this.dynamic = Container.instance.get("Component");
+        this.checkResolve();
     }
 
-    async attached() {
-        console.log(this.gustVm);
+    /**
+     * Called by framework
+     */
+    bind() {
         let template = `
             <template>
-              <${this.gustVm.tagName} 
+              <${this.dynamic.tagName} 
                 name.two-way="name"
-                log.call="log()"
-              ></${this.gustVm.tagName}>
+                log.call="log($event)"
+                view-model.ref="instance"
+                view.ref="view"
+                ref="element"
+              ></${this.dynamic.tagName}>
             </template>`;
-        let res = await this.viewEngine.importViewResources([this.gustVm.resource], [], this.viewResources);
-        this.viewFactory.insert(this.host, template, this, res);
+        return this.viewEngine.importViewResources([this.dynamic.resource], [], this.viewResources).then(viewResources => {
+            this.remove = this.viewFactory.insert(this.host, template, this, viewResources);
+            this.taskQueue.queueMicroTask(this.onDatabound);
+        });
+    }
+
+    /**
+     * Checks that the resolved guest component instance is valid. Throws a TypeError otherwise
+     */
+    private checkResolve() {
+        if (!this.dynamic)
+            throw new TypeError("[Component] Trying to resolve a guest component that is not registred");
+
+        if (!this.dynamic.resource || this.dynamic.resource.length == 0)
+            throw new TypeError("[Component] Trying to resolve a guest component that has not a valid resource value");
+
+        if (!this.dynamic.tagName || this.dynamic.tagName.length == 0)
+            throw new TypeError("[Component] Trying to resolve a guest component that has not a valid tag name");
+    }
+
+    /**
+     * Called by framework when the component is detached from the DOM
+     */
+    detached() {
+        this.remove();
     }
 }
